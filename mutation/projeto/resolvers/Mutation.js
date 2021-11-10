4e370726c50bb015b3a108a700bf51f23aa80879
Tiago Oliveira <tiagolivesan@outlook.com>
2021-11-09 22:39:44 -0300

    Curso GraphQL

---
+++ mutation/projeto/resolvers/Mutation.js
@@ -0,0 +1,28 @@
+const { usuarios, proximoId } = require('../data/db')
+
+module.exports = {
+  // nome, email, idade
+  novoUsuario(_, { args }) {
+
+    const emailExistente = usuarios.some(u => u.email === args.email)
+
+    if (emailExistente) {
+      throw new Error('Email cadastrado')
+    }
+
+    const novo = {
+      id: proximoId(),
+      ...args,
+      perfil_id: 1,
+      status: 'ATIVO'
+    }
+    usuarios.push(novo)
+    return novo
+  },
+  excluirUsuario(_, { id }) {
+    const i = usuarios.findIndex(u => u.id === id)
+    if (i < 0) return null
+    const excluidos = usuarios.splice(i, 1)
+    return excluidos ? excluidos[0] : null
+  }
+}
