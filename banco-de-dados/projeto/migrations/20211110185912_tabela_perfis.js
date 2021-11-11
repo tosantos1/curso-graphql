64fd7fbae737f458d3f442dcf395cf8c307ea88e
Tiago Oliveira <tiagolivesan@outlook.com>
2021-11-11 19:51:59 -0300

    Curso GraphQL

--- banco-de-dados/projeto/migrations/20211110185912_tabela_perfis.js
+++ banco-de-dados/projeto/migrations/20211110185912_tabela_perfis.js
@@ -1,8 +1,12 @@
 
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.createTable('perfis', table => {
+    table.increments('id').primary();
+    table.string('nome').notNull().unique();
+    table.string('rotulo').notNull()
+  })
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTable('perfis')
 };
